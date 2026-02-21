35064dafb46200f462f14d4f5afc3a6c924a23ec
Rishi Vhavle <rishivhavle21@gmail.com>
2026-02-22 02:01:51 +0530

    chore: add resync script

---
+++ be/scripts/resync.ts
@@ -0,0 +1,10 @@
+import { syncRegistrations } from "../src/jobs/vtopiaSync.js";
+
+async function main() {
+  console.log("Running manual sync...");
+  await syncRegistrations();
+  console.log("Done.");
+  process.exit(0);
+}
+
+main().catch(console.error);
